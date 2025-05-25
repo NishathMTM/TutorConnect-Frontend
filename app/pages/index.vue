cabd937f5b2e085a0fa08c18afce2b75c6b86dde
Nasrin24 <mtf.nasrin@gmail.com>
2025-05-25 23:20:00 +0530

    update home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,16 +1,87 @@
 <script setup lang="ts">
+import PublicLayout from '~/layout/public-layout.vue';
 
+/* ---------------------------------------------------------------------------------------------- */
+
+/* ---------------------------------------------------------------------------------------------- */
+/*
+ * Path: /
+ * Description: Homepage
+ */
+
+useAppTitle('Welcome');
+
+/* ---------------------------------------------------------------------------------------------- */
 </script>
 
 <template>
-   <div>
-      <h1 class="font-light sm:text-4xl ">
-         Welcome Nuxt content from pages index
-      </h1>
-      <p class="text-green-700" />
-   </div>
+   <PublicLayout>
+      <!-- region: body -->
+      <section class="p-4 lg:p-0">
+         <div class="flex w-full flex-col lg:flex-row lg:gap-5">
+            <aside class="basis-2/5">
+               <div class="mb-3 text-center font-bold text-first-500 lg:text-left">
+                  Bridging the Gap Between Teaching and Learning.
+               </div>
+
+               <h4 class=" mt-5 mb-5 text-4xl justify-center font-bold text-primaryColor lg:text-5xl">
+                  Experience the Future. Start Managing Your learning and teaching Smarter – Try Now!
+               </h4>
+
+               <div class="text-lg font-bold text-primaryColor">
+                  "Stay Connected with Your Tutors Anytime, Anywhere"
+               </div>
+
+               <div class="mt-10 text-center lg:text-left">
+                  <UButton
+                     to="/register"
+                     size="xl"
+                     :ui="{
+                        base: 'shadow-[0px_10px_17px_3px_#c3dafe] hover:shadow-[0px_10px_17px_3px_#a3bffa] transition-all',
+                        font: 'font-black uppercase',
+                        padding: {
+                           xl: 'px-10',
+                        },
+                     }"
+                  >
+                     Register now
+                  </UButton>
+               </div>
+            </aside>
+            <!-- region: image -->
+            <div class="mt-10 basis-3/5 lg:mt-0">
+               <div class="max-h-[380px]">
+                  <img
+                     class="max-h-[380px] w-full rounded-xl object-cover drop-shadow"
+                     src="/img.png"
+                     alt=""
+                  >
+               </div>
+            </div>
+            <!-- endregion: image -->
+         </div>
+      </section>
+      <!-- endregion: body -->
+
+      <!-- region: property listings -->
+      <div class="mt-10 p-4 lg:p-0">
+         <header class="mb-5">
+            <Heading1>Recently listed Classes</Heading1>
+         </header>
+
+         <!--         <PublicCourseListings /> -->
+
+         <footer class="mt-5 flex justify-center">
+            <NuxtLink
+               class="text-lg font-bold uppercase text-first-600 hover:text-first-700"
+               to="/public/listings"
+            >
+               Check out more listings
+            </NuxtLink>
+         </footer>
+      </div>
+      <!-- endregion: property listings -->
+   </PublicLayout>
 </template>
 
-<style scoped>
-
-</style>
+<style lang="postcss"></style>
